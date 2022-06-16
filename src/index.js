62492d4879babee41c920702e8ba5972ee82288c
clementmenc <contact.clementm@gmail.com>
2022-06-16 20:21:27 +0200

    Add base of global style

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './styles/main.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
